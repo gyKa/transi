23a9d4e80ef65af6fc11253c0dcd0237f9be7de4
Gytis Karčiauskas <gytis@karciauskas.lt>
2014-08-19 12:10:24 +0300

    Show vehicle's statistic.

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -28,4 +28,28 @@ $app->get('/', function () use ($app) {
     ]);
 });
 
+$app->get('/vehicles/{id}', function ($id) use ($app) {
+	// Use LEFT JOIN, because there can be exists a vehicle without any trips.
+    $vehicle = $app['db']->fetchAssoc(
+        'SELECT
+            SUM(IF(trips.date >= DATE_SUB(NOW(), INTERVAL 7 DAY), trips.distance, 0)) as total_distance_week,
+            SUM(IF(trips.date >= DATE_SUB(NOW(), INTERVAL 4 WEEK), trips.distance, 0)) as total_distance_month,
+            SUM(IF(trips.date >= DATE_SUB(NOW(), INTERVAL 365 DAY), trips.distance, 0)) as total_distance_year,
+            IFNULL(SUM(trips.distance), 0) as total_distance
+        FROM vehicles
+        LEFT JOIN trips ON vehicles.id = trips.vehicle_id
+        WHERE vehicles.id = ?',
+        [(int)$id]
+    );
+
+    // If vehicle doesn't exists, this value is NULL.
+    if (is_null($vehicle['total_distance_week'])) {
+        $app->abort(404, 'Vehicle does not exist!');
+    }
+
+    return $app['twig']->render('vehicle.twig', [
+        'vehicle' => $vehicle,
+    ]);
+});
+
 return $app;

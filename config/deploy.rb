84710fdba1977306f6e400fcf1f4b0212b93622b
Gytis Karčiauskas <gytis@karciauskas.lt>
2014-08-28 23:10:58 +0300

    Install Composer in deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :deploy_to, '/var/www/transi'
 # set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{vendor}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -36,17 +36,16 @@ set :deploy_to, '/var/www/transi'
 
 namespace :deploy do
 
-  desc 'Restart application'
-  task :restart do
+  desc 'Install Composer'
+  task :install_composer do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :curl, release_path.join('-sS https://getcomposer.org/installer | php')
     end
   end
 
-  after :publishing, :restart
+  after :publishing, :install_composer
 
-  after :restart, :clear_cache do
+  after :install_composer, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       # within release_path do

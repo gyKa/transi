6850d9741af953e0724e6b7abd3f3d81882ec273
Gytis Karčiauskas <gytis@karciauskas.lt>
2014-08-28 08:01:52 +0300

    Config Capistrano.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'transi'
+set :repo_url, 'https://github.com/gyKa/transi.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/var/www/transi'
 
 # Default value for :scm is :git
 # set :scm, :git
